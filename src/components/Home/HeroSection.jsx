2d76e084785094a2ec301b17b7abe39908159fe6
jaimeenbhagat <jaimeenbhagat@outlook.com>
2025-06-30 16:54:15 +0400

    Updated hero

--- src/components/Home/HeroSection.jsx
+++ src/components/Home/HeroSection.jsx
@@ -4,7 +4,7 @@ import bgImage from "../../assets/herosectionimage1.png"; // Your new image
 const HeroSection = () => {
   return (
     <section
-      className="relative w-full min-h-screen bg-black text-white flex items-center justify-center px-6 sm:px-10 md:px-20 pt-24 pb-16 md:pt-32 md:pb-24 bg-cover bg-center"
+      className="relative w-full min-h-screen bg-black text-white flex items-center justify-center px-6 sm:px-10 md:px-20 pt-24 pb-16 md:pt-32 md:pb-24 bg-cover bg-center font-interphase"
       style={{
         backgroundImage: `url(${bgImage})`,
         backgroundRepeat: "no-repeat",
@@ -17,19 +17,19 @@ const HeroSection = () => {
 
       {/* Text Content */}
       <motion.div
-        className="relative z-10 max-w-4xl text-center"
+        className="relative z-10 max-w-5xl text-center"
         initial={{ opacity: 0, y: 20 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 1 }}
       >
-        <h1 className="font-satoshi text-2xl sm:text-3xl md:text-4xl lg:text-5xl text-center font-extrabold leading-tight mb-4 sm:mb-6">
-          Everything You Need for Team Building & Employee Engagement.
+        <h1 className=" text-2xl sm:text-3xl md:text-4xl lg:text-5xl text-center font-extrabold leading-tight mb-4 sm:mb-6">
+          Everything You Need for <br /> Team Building & Employee Engagement.
           <br className="hidden sm:block" />
           One Roof. One Thrive.
         </h1>
 
-        <p className="text-base sm:text-lg md:text-xl text-white text-center mb-6 sm:mb-8 font-medium leading-relaxed max-w-4xl mx-auto">
-          At OneThrive, we create meaningful, memorable experiences that bring your teams closer. 
+        <p className="text-base sm:text-lg md:text-xl text-white text-center mb-6 sm:mb-8 font-medium leading-relaxed max-w-3xl mx-auto">
+          At OneThrive, we create meaningful, memorable experiences that bring your teams <br /> closer. 
           Whether you're scaling culture or boosting collaboration, we deliver everything you need 
           to help your people connect, grow, and thrive.
         </p>
